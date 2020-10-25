ada08b708ce8d680111b6e4f8f5d9181c122d852
Aaron Lewis <aarondev8917@gmail.com>
2020-10-25 17:24:29 +0530

    Added validation Rules for Request

--- app/Http/Middleware/ResponseCacheMiddleware.php
+++ app/Http/Middleware/ResponseCacheMiddleware.php
@@ -4,6 +4,8 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Cache;
+use Illuminate\Support\Facades\Validator;
+use App\Rules\Lowercase;
 
 class ResponseCacheMiddleware
 {
@@ -16,6 +18,23 @@ class ResponseCacheMiddleware
      */
     public function handle($request, Closure $next)
     {
+
+        $validator = Validator::make($request->all(), [
+            'activity' => 'required|integer|gt:0',
+            'activityType' => 'required|string',
+            'country' => ['required', 'string', new Lowercase]
+        ]);
+
+        if ($validator->fails()) {
+            $errors = $validator->errors();
+            foreach ($errors->all() as $message) {
+                return response()->json([
+                    'error' => true,
+                    'message' => $message
+                 ],200); 
+            }
+        }
+
         $key = '|request|'.$request->url().'|'. 'activity-' . $request->input('activity') . '|activityType-' . $request->input('activityType') . '|country-' . $request->input('country') . '|fuelType-' . $request->input('fuelType') . '|mode-' . $request->input('mode') ;
         return Cache::store('database')->remember($key, 24 * 60 * 60 , function () use($request, $next) {
             return $next($request);
